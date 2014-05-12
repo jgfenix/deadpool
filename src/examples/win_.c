0438c1651875ab28c107874bd50f4e124d26d8d3
Joao Gabriel <gabriel@expertisesolutions.com.br>
2014-05-12 16:22:42 -0300

    :q

--- src/examples/win_.c
+++ src/examples/win_.c
@@ -20,27 +20,27 @@ static void
        evas_object_del(in_win);
     }
 
-static void//inwin mode 'on'
+static void//inwin mode 'on' to show the content of app button
    b_inw_cb(void *data, Evas_Object *previous_win, void *event_info)
    {
     
-    Evas_Object  *h_box, *ic, *b_back, *bt ;
+    Evas_Object  *inw_box, *ic, *b_back, *bt ;
     
     _win = elm_object_top_widget_get(previous_win);//get the previous window as parameter
       
     in_win = elm_win_inwin_add(_win); 
-    evas_object_show(in_win);//OR elm_win_inwin_activate(in_win);
-        
-    h_box = elm_box_add(_win);
-    elm_box_horizontal_set(h_box, EINA_TRUE);
-    evas_object_size_hint_weight_set(h_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
-    elm_win_resize_object_add(_win, h_box);
-    elm_box_padding_set(h_box, 1, 1);
-    evas_object_show(h_box);
+    elm_win_inwin_activate(in_win);//evas_object_show(in_win);
+       
+    inw_box = elm_box_add(_win);
+    elm_box_horizontal_set(inw_box, EINA_TRUE);
+    evas_object_size_hint_weight_set(inw_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
+    elm_win_resize_object_add(_win, inw_box);
+    elm_box_padding_set(inw_box, 1, 1);
+    evas_object_show(inw_box);
 
-    b_back = elm_button_add(h_box);
+    b_back = elm_button_add(inw_box);
     elm_object_text_set(b_back, " back ");
-    elm_box_pack_end(h_box, b_back);
+    elm_box_pack_end(inw_box, b_back);
     evas_object_show(b_back);
     evas_object_smart_callback_add(b_back, "clicked", back_win, NULL);
         
@@ -49,10 +49,10 @@ static void//inwin mode 'on'
     elm_icon_standard_set(ic, "chat");
     evas_object_show(ic);
 
-    bt = elm_button_add(h_box);
+    bt = elm_button_add(inw_box);
     elm_object_text_set(bt, "chat ");
     elm_object_part_content_set(bt, "icon", ic);
-    elm_box_pack_end(h_box, bt);
+    elm_box_pack_end(inw_box, bt);
     evas_object_show(bt);
     //evas_object_smart_callback_add(bt, "clicked", kill_bt, NULL);
 
@@ -61,10 +61,10 @@ static void//inwin mode 'on'
     elm_icon_standard_set(ic, "clock");
     evas_object_show(ic);
 
-    bt = elm_button_add(h_box);
+    bt = elm_button_add(inw_box);
     elm_object_text_set(bt, "Clock");
     elm_object_part_content_set(bt, "icon", ic);
-    elm_box_pack_end(h_box, bt);
+    elm_box_pack_end(inw_box, bt);
     evas_object_show(bt);
     //evas_object_smart_callback_add(bt, "clicked", kill_bt, NULL);
 
@@ -73,25 +73,23 @@ static void//inwin mode 'on'
     elm_icon_standard_set(ic, "arrow_right");
     evas_object_show(ic);
 
-    bt = elm_button_add(h_box);
+    bt = elm_button_add(inw_box);
     elm_object_text_set(bt, "Player ");
     elm_object_part_content_set(bt, "icon", ic);
-    elm_box_pack_end(h_box, bt);
+    elm_box_pack_end(inw_box, bt);
     evas_object_show(bt);
            
-    elm_win_inwin_content_set(in_win, h_box);  
+    elm_win_inwin_content_set(in_win, inw_box);  
    }
 
 
-void//make 'buttons' with File and print the file in terminal
+static void//make 'buttons' with File and print the file in terminal
    _file_chosen(void *data, Evas_Object *obj, void *event_info)
   {  
    Evas_Object *bt;
    const char *file = event_info;
    
-   //_win = elm_object_top_widget_get(obj);
-   
-     if (file!= NULL)
+    if (data!= NULL)
      {     
        bt = elm_button_add(_win);
        elm_object_text_set(bt, file);
@@ -104,17 +102,55 @@ void//make 'buttons' with File and print the file in terminal
      printf("File selection canceled.\n");
   }
 
-void//naviframe mode
-  n_frame(void *data, Evas_Object *obj, void *event_info)
+static void//second naviframe 
+  n_frame_2(void *data, Evas_Object *obj, void *event_info)
   {
-   Evas_Object *nv = data, *ic, *fs_bt, *en, *h_box, *b_n;
+   Evas_Object *nv = data, *ic, *bt, *nv_box, *b_exit;
    
+  if(data != NULL)
+   { 
+   nv_box = elm_box_add(nv);
+   elm_box_horizontal_set(nv_box, EINA_FALSE);
+   evas_object_size_hint_weight_set(nv_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
+   elm_win_resize_object_add(nv, nv_box);
+   evas_object_show(nv_box);
 
-   h_box = elm_box_add(nv);
-   elm_box_horizontal_set(h_box, EINA_FALSE);
-   evas_object_size_hint_weight_set(h_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
-   elm_win_resize_object_add(nv, h_box);
-   evas_object_show(h_box);
+   ic = elm_icon_add(nv);
+   elm_icon_order_lookup_set(ic, ELM_ICON_LOOKUP_THEME_FDO);
+   elm_icon_standard_set(ic, "edit");
+   evas_object_show(ic);
+  
+   bt = elm_button_add(nv);
+   elm_object_text_set(bt, "Write");
+   elm_object_part_content_set(bt, "icon", ic);
+   elm_box_pack_end(nv_box, bt);
+   evas_object_size_hint_min_set(bt, 100, 100);
+   evas_object_show(bt);
+
+   b_exit = elm_button_add(nv);
+   elm_object_text_set(b_exit, " EXIT ");
+   elm_box_pack_end(nv_box, b_exit);
+   evas_object_show(b_exit);
+   evas_object_smart_callback_add(b_exit, "clicked", exit_program, NULL);
+
+
+  elm_naviframe_item_push(nv, NULL, NULL, NULL, nv_box, NULL);
+   //evas_object_smart_callback_add(b_next, "clicked", n_frame_2, nv);
+   }
+  }
+
+static void//'first' naviframe mode
+  n_frame_1(void *data, Evas_Object *obj, void *event_info)
+  {
+   Evas_Object *nv = data, *ic, *fs_bt, *en, *nv_box, *b_next, *b_exit;
+   
+  if(data != NULL)
+   {
+   nv_box = elm_box_add(nv);
+   elm_box_horizontal_set(nv_box, EINA_FALSE);
+   evas_object_size_hint_weight_set(nv_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
+   elm_win_resize_object_add(nv, nv_box);
+   evas_object_show(nv_box);
 
    ic = elm_icon_add(nv);
    elm_icon_order_lookup_set(ic, ELM_ICON_LOOKUP_THEME_FDO);
@@ -126,7 +162,7 @@ void//naviframe mode
    elm_object_text_set(fs_bt, "Select a file");
    elm_object_part_content_set(fs_bt, "icon", ic);
    elm_fileselector_button_inwin_mode_set(fs_bt, EINA_TRUE);
-   elm_box_pack_end(h_box, fs_bt);
+   elm_box_pack_end(nv_box, fs_bt);
    evas_object_size_hint_min_set(fs_bt, 100, 100);
    evas_object_show(fs_bt);
    
@@ -134,7 +170,7 @@ void//naviframe mode
    elm_entry_line_wrap_set(en, EINA_FALSE);
    elm_entry_editable_set(en, EINA_FALSE);
    evas_object_smart_callback_add(fs_bt, "file,chosen", _file_chosen, en);
-   elm_box_pack_end(h_box, en);
+   elm_box_pack_end(nv_box, en);
    evas_object_show(en);
 
    ic = elm_icon_add(nv);
@@ -142,25 +178,32 @@ void//naviframe mode
    elm_icon_standard_set(ic, "arrow_right");
    evas_object_show(ic);
 
-   b_n = elm_button_add(nv);
-   elm_object_text_set(b_n, "Next");
-   elm_object_part_content_set(b_n, "icon",ic);
-   elm_box_pack_end(h_box, b_n);
-   evas_object_size_hint_min_set(b_n, 60, 40); 
-   evas_object_show(b_n);
-   evas_object_smart_callback_add(b_n, "clicked", n_frame, nv);
-  
-   elm_naviframe_item_push(nv, NULL, NULL, b_n, h_box, NULL);
+   b_next = elm_button_add(nv);
+   elm_object_text_set(b_next, "Next");
+   elm_object_part_content_set(b_next, "icon",ic);
+   elm_box_pack_end(nv_box, b_next);
+   evas_object_size_hint_min_set(b_next, 60, 40); 
+   evas_object_show(b_next);
+   evas_object_smart_callback_add(b_next, "clicked", n_frame_2, nv);
+ 
+   b_exit = elm_button_add(nv);
+   elm_object_text_set(b_exit, " EXIT ");
+   elm_box_pack_end(nv_box, b_exit);
+   evas_object_show(b_exit);
+   evas_object_smart_callback_add(b_exit, "clicked", exit_program, NULL);
+
+    
+   elm_naviframe_item_push(nv, NULL, NULL, b_next, nv_box, NULL);
    //elm_naviframe_item_push(nv, NULL, NULL, NULL, fs_bt, NULL);//expand the icon if you don't pack fs_bt
-  
    }
+  }
 
 
 EAPI_MAIN int
 
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win, *nv, *box, *t_box, *ic,  *b_n, *b_exit, *b_app ;
+   Evas_Object *win, *nv, *box, *app_box, *ic,  *b_next, *b_exit, *b_app ;
 
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);  
 
@@ -203,25 +246,25 @@ elm_main(int argc, char **argv)
    elm_win_resize_object_add(nv, box);
    evas_object_show(box);
  
-   t_box = elm_box_add(nv);
-   elm_box_horizontal_set(t_box, EINA_TRUE);
-   elm_box_homogeneous_set(t_box, EINA_TRUE);
-   evas_object_size_hint_weight_set(t_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
-   elm_win_resize_object_add(win, t_box);
-   evas_object_show(t_box);
+   app_box = elm_box_add(nv);
+   elm_box_horizontal_set(app_box, EINA_TRUE);
+   elm_box_homogeneous_set(app_box, EINA_TRUE);
+   evas_object_size_hint_weight_set(app_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
+   elm_win_resize_object_add(win, app_box);
+   evas_object_show(app_box);
 
    ic = elm_icon_add(nv);
    elm_icon_order_lookup_set(ic, ELM_ICON_LOOKUP_THEME_FDO);
    elm_icon_standard_set(ic, "arrow_right");
    evas_object_show(ic);
 
-   b_n = elm_button_add(nv);
-   elm_object_text_set(b_n, "Next");
-   elm_object_part_content_set(b_n, "icon",ic);
-   elm_box_pack_end(box, b_n);
-   evas_object_size_hint_min_set(b_n, 60, 40); 
-   evas_object_show(b_n);
-   evas_object_smart_callback_add(b_n, "clicked", n_frame, nv);
+   b_next = elm_button_add(nv);
+   elm_object_text_set(b_next, "Next");
+   elm_object_part_content_set(b_next, "icon",ic);
+   elm_box_pack_end(box, b_next);
+   evas_object_size_hint_min_set(b_next, 60, 40); 
+   evas_object_show(b_next);
+   evas_object_smart_callback_add(b_next, "clicked", n_frame_1, nv);
    
    b_exit = elm_button_add(nv);
    elm_object_text_set(b_exit, " EXIT ");
@@ -238,12 +281,13 @@ elm_main(int argc, char **argv)
    b_app = elm_button_add(nv);
    elm_object_text_set(b_app, "APPS");
    elm_object_part_content_set(b_app, "icon",ic);
-   elm_box_pack_end(t_box, b_app);
+   elm_box_pack_end(app_box, b_app);
    evas_object_size_hint_min_set(b_app, 100, 100); 
    evas_object_show(b_app);
    evas_object_smart_callback_add(b_app, "clicked", b_inw_cb, NULL);
   
-   elm_naviframe_item_push(nv, NULL, NULL, box, b_app, NULL);
+   elm_naviframe_item_push(nv, NULL, NULL, box, b_app, NULL);//'zero' naviframe mode
+   
    
    elm_run();
    elm_shutdown();
