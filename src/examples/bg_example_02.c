15f0b6bc8defb25ba603abde521803bad36bdef6
Joao Gabriel <gabriel@expertisesolutions.com.br>
2014-05-15 16:51:33 -0300

    :q

--- src/examples/bg_example_02.c
+++ src/examples/bg_example_02.c
@@ -7,27 +7,47 @@
 EAPI_MAIN int
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win, *bg ;
+   Evas_Object *win, *box, *bg, *layout, *bt;
    char buf[PATH_MAX];
 
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
    elm_app_info_set(elm_main, "elementary", "images/cube1.png");// I created a directory in /usr/local/share/elementary/images and changed the image
       
-   win = elm_win_add(NULL, "bg-image", ELM_WIN_BASIC);
+   win = elm_win_util_standard_add("win", "Win"); //elm_win_add(NULL, "bg-image",ELM_WIN_BASIC);
    elm_win_title_set(win, "Bg Image");
    elm_win_autodel_set(win, EINA_TRUE);
    evas_object_resize(win, 320, 320);
    evas_object_show(win);
    
+   box = elm_box_add(win);
+   elm_box_horizontal_set(box, EINA_TRUE);
+   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
+   elm_win_resize_object_add(win, box);
+   evas_object_show(box);
+   
+  /* layout = elm_layout_add(win);
+   evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, layout);
+   elm_layout_text_set(layout, buf, "TESTE");
+   evas_object_show(layout);*/
+ 
    bg = elm_bg_add(win);
+
    elm_bg_load_size_set(bg, 20, 20);
    //elm_bg_option_set(bg, ELM_BG_OPTION_STRETCH);//see elm_bg_common.h
    snprintf(buf, sizeof(buf), "%s/images/cube1.png", elm_app_data_dir_get());
    elm_bg_file_set(bg, buf, NULL);
    evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, bg);// see elm_win_legacy.h 
-   evas_object_show(bg);      
+   evas_object_show(bg); 
 
+   bt = elm_button_add(win);
+   elm_object_text_set(bt, " click ");
+   elm_box_pack_start(box, bt);
+   evas_object_size_hint_min_set(bt, 60, 40); 
+   evas_object_show(bt);
+
+   //evas_object_show(layout);
     
    elm_run();
    elm_shutdown();
--- src/examples/win_.c
+++ src/examples/win_.c
@@ -203,8 +203,8 @@ EAPI_MAIN int
 
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win, *nv, *box, *app_box, *ic, *b_next, *b_exit, *b_app ;
-
+   Evas_Object *win, *image, *nv, *box, *app_box, *ic, *b_next, *b_exit, *b_app ;
+   char buf[PATH_MAX];
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);  
 
 /*-----  EXTERN WINDOW -------------------------------------------------------------*/
@@ -234,14 +234,36 @@ elm_main(int argc, char **argv)
    evas_object_resize(win, 400, 300);
    evas_object_show(win);
 
+   elm_app_info_set(elm_main, "elementary", "images/plant_01.jpg");
+   snprintf(buf, sizeof(buf), "%s/images/plant_01.jpg", elm_app_data_dir_get());
+
+   image = elm_image_add(win);
+   if (!elm_image_file_set(image, buf, NULL))
+     {
+        printf("error: could not load image \"%s\"\n", buf);
+        return -1;
+     }
+
+   elm_image_no_scale_set(image, EINA_TRUE);
+   elm_image_resizable_set(image, EINA_FALSE, EINA_TRUE);
+   elm_image_smooth_set(image, EINA_FALSE);
+   elm_image_orient_set(image, ELM_IMAGE_FLIP_HORIZONTAL);
+   elm_image_aspect_fixed_set(image, EINA_TRUE);
+   elm_image_fill_outside_set(image, EINA_TRUE);
+   elm_image_editable_set(image, EINA_TRUE);
+
+   evas_object_size_hint_weight_set(image, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, image);
+   evas_object_show(image);  
+
    nv = elm_naviframe_add(win);
    evas_object_size_hint_weight_set(nv, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, nv);
    evas_object_show(nv);  
 
+   
    box = elm_box_add(nv);
    elm_box_horizontal_set(box, EINA_TRUE);
-   elm_box_homogeneous_set(box, EINA_TRUE);
    evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
    elm_win_resize_object_add(nv, box);
    evas_object_show(box);
