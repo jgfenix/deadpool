a1a33b7451b466ac29228474337addfe5d65db1c
Joao Gabriel <gabriel@expertisesolutions.com.br>
2014-06-02 15:45:25 -0300

    :q

--- src/examples/an.c
+++ src/examples/an.c
@@ -5,6 +5,7 @@
 */ 
 
 #include <Elementary.h>
+Evas_Object *panes, *winM;
 
 static void
    _file_chosen(void *data, Evas_Object *obj, void *selected_item);
@@ -64,13 +65,9 @@ static void
 
   char file[PATH_MAX];
   char buf[PATH_MAX];
-//  char test[15];
- // memset(test, 15 , '\0');
 
   DIR *dir;
   struct dirent *lsdir; 
-
-  //unsigned char i, count = 0; 
 
   elm_need_ethumb();
         
@@ -99,42 +96,7 @@ static void
          if (!elm_image_file_set(image, buf, NULL))
             snprintf(buf, sizeof(buf), "%s","/usr/share/elementary/images/icon_06.png");
 
-/*  THIS PART DO THE SAME AS THE TEST IN elm_image_file_set() */      
-      /*  for(i = 0; i < strlen(file) ; i++)
-          {
-             if( file[i] != '.') 
-               count ++;
-            
-             else
-               break;
-          };   
 
-        printf("CHARACTERS BEFORE FIRST '.' = %d\n", count);
-        
-        count = count +1;
-
-      if( (strlen(file) - count) != count )
-       {
-
-        for(i = 0; i < 15 || count < 15 ; i++, count++ )
-             test[i] = file[count] ;
-
-        printf("TYPE OF FILE == %s\n",test);
-        printf("STRLEN(TYPE_OF_FILE) == %ld\n",strlen(test));
-        count = 0;
-        
-       if(!strncmp(test, "jpg", strlen("jpg")) || !strncmp(test, "jpeg", strlen("jpeg"))  || !strncmp(test, "png", strlen("png")) )
-            printf("%s File -> valid type of image\n",test);
-       else
-         {
-            printf("* %s not a image file -> changing original path for generic path\n", test);
-            snprintf(buf, sizeof(buf), "%s","/usr/local/share/elementary/images/generic_thumb.png" );
-         };
-       } 
-
-      else
-           printf("TYPE OF FILE == folder\n");
-       */
         printf("PATH == %s\n\n", buf);
               
         elm_thumb_editable_set(thumb, EINA_FALSE);
@@ -148,10 +110,8 @@ static void
         evas_object_size_hint_min_set(bt, 100, 100);
         evas_object_show(bt);
 
-        //evas_object_smart_callback_add(bt, "clicked", image_view, th_win);
         elm_box_recalculate(th_box);  
             
-        //memset(test, 15 , '\0');
       }; 
 
      closedir(dir);
@@ -167,11 +127,13 @@ static void
    char *file = selected_item;  
    int w, h, hg;
 
+    win = elm_win_util_standard_add("image",file);
+    elm_win_autodel_set(win, EINA_TRUE);
+    evas_object_resize(win, 600, 600);
+
    if (file != NULL)
      {   
         list(file);
-        win = elm_win_util_standard_add("image", "Image");
-        elm_win_autodel_set(win, EINA_TRUE); 
         image = elm_image_add(win);
           
         if (!elm_image_file_set(image, file, NULL))
@@ -190,19 +152,20 @@ static void
            printf("\nwidth x height = %d x %d\n", w, h);
            
            elm_image_no_scale_set(image, EINA_FALSE);
+           elm_image_prescale_set(image, hg);  
            elm_image_resizable_set(image, EINA_TRUE, EINA_TRUE);
            elm_image_smooth_set(image, EINA_TRUE);
            elm_image_orient_set(image, ELM_IMAGE_ORIENT_NONE);
            elm_image_aspect_fixed_set(image, EINA_FALSE);
            elm_image_fill_outside_set(image, EINA_TRUE);
            elm_image_editable_set(image, EINA_TRUE);
-           
-           elm_image_prescale_set(image, hg);  
-           elm_win_resize_object_add(win, image);
            evas_object_size_hint_weight_set(image, EVAS_HINT_FILL, EVAS_HINT_FILL);
-           evas_object_resize(win, 200, 200);
+           evas_object_show(image);
+           
+           elm_win_resize_object_add(win, image);
            evas_object_show(win);
            evas_object_show(image);
+           //elm_object_part_content_set(panes, "right", image);
          }
         else
           printf("* Invalid path or image *\n\n");
@@ -220,6 +183,7 @@ static void
       elm_thumb_file_set(thumb, path, NULL);
       elm_thumb_reload(thumb);
       evas_object_show(thumb);
+      elm_object_part_content_set(panes, "right", thumb);
    }   
 
 
@@ -227,63 +191,64 @@ EAPI_MAIN int
 
 elm_main(int argc, char **argv)
 {
-   Evas_Object *win, *thumb, *box, *ic, *fs_bt, *en, *b_exit ;
+   Evas_Object *thumb, *box, *ic, *fs_bt, *en ;
    
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);  
   
   elm_need_ethumb();
-    win = elm_win_util_standard_add("image/thumb", "Image_&_Thumb_Generator");
-    evas_object_smart_callback_add(win, "delete,request", exit_cb, NULL);
-    elm_win_autodel_set(win, EINA_TRUE);
-    evas_object_resize(win, 600, 800);
-    evas_object_show(win);
+    winM = elm_win_util_standard_add("image/thumb", "Image_&_Thumb_Generator");
+    evas_object_smart_callback_add(winM, "delete,request", exit_cb, NULL);
+    elm_win_autodel_set(winM, EINA_TRUE);
+    evas_object_resize(winM, 600, 600);
+    evas_object_show(winM);
+    
+    panes = elm_panes_add(winM);
+    evas_object_size_hint_weight_set(panes, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    elm_win_resize_object_add(winM, panes);
+    evas_object_show(panes);
 
-    thumb = elm_thumb_add(win);
+    thumb = elm_thumb_add(winM);
          
     elm_thumb_reload(thumb);
     evas_object_smart_callback_add(thumb, "generate,start", _generation_started_cb, NULL);
     evas_object_smart_callback_add(thumb, "generate,stop", _generation_finished_cb, NULL);
     evas_object_smart_callback_add(thumb, "generate,error", _generation_error_cb, NULL);
 
-    box = elm_box_add(win);
+    box = elm_box_add(winM);
     elm_box_horizontal_set(box, EINA_TRUE);
     evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); 
-    elm_win_resize_object_add(win, box);
+    elm_win_resize_object_add(winM, box);
     elm_box_padding_set(box, 1, 1);
     elm_box_homogeneous_set(box,EINA_TRUE);
     evas_object_show(box);
    
-    ic = elm_icon_add(win);
+    ic = elm_icon_add(winM);
     elm_icon_order_lookup_set(ic, ELM_ICON_LOOKUP_THEME_FDO);
     elm_icon_standard_set(ic, "menu/folder");
     evas_object_show(ic);
   
-    fs_bt = elm_fileselector_add(win);
+    fs_bt = elm_fileselector_add(winM);
     elm_fileselector_expandable_set(fs_bt, EINA_TRUE);
     elm_fileselector_path_set(fs_bt, "/home");
     elm_object_text_set(fs_bt, "Select a file");
     evas_object_size_hint_weight_set(fs_bt, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     evas_object_size_hint_align_set(fs_bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-    elm_box_pack_end(box, fs_bt);
+    //elm_box_pack_end(box, fs_bt);
     //elm_fileselector_button_inwin_mode_set(fs_bt, EINA_TRUE);
-    evas_object_smart_callback_add(fs_bt, "selected", _selected_file, win);
+    evas_object_smart_callback_add(fs_bt, "selected", _selected_file,NULL);
     evas_object_smart_callback_add(fs_bt, "done", _file_chosen, en);
     evas_object_show(fs_bt);
     
-    en = elm_entry_add(win);
+    en = elm_entry_add(winM);
     elm_entry_line_wrap_set(en, EINA_FALSE);
     elm_entry_editable_set(en, EINA_FALSE);
     evas_object_smart_callback_add(fs_bt, "file,chosen", _file_chosen, en);
     elm_box_pack_end(box, en);
     evas_object_show(en);
 
-    b_exit = elm_button_add(win);
-    elm_object_text_set(b_exit, "  EXIT  ");
-    elm_box_pack_end(box, b_exit);
-    evas_object_show(b_exit);
-    evas_object_smart_callback_add(b_exit, "clicked", exit_cb, NULL);
-
-   elm_run();
+    elm_object_part_content_set(panes, "left", fs_bt);
+   
+    elm_run();
    elm_shutdown();
    return 0;
 }
